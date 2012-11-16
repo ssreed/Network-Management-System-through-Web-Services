8e73c2bfaed98a363c379517f7f49a579b453511
Sri Sreed <srisreed@gmail.com>
2012-11-15 18:16:59 -0800

    added show all button

--- js/core.js
+++ js/core.js
@@ -3,7 +3,7 @@ $('li').click(function(){this.element.find('ul').first().toggle()}) //work in pr
 */
 
 $(document).ready(function(){ 
-	/*generate tree*/
+	/*dummy data*/
     var jsonArray = [
         {'name' : 'iso(1)','parent' : '', 'oid' : '1'},
         {'name' : 'org(3)','parent' : 'iso\\(1\\)', 'oid' : '1.3'},
@@ -12,32 +12,34 @@ $(document).ready(function(){
         {'name' : 'mgmt(2)',  'parent' : 'internet\\(1\\)', 'oid' : '1.3.6.1.2'},
         {'name' : 'mib(2)',  'parent' : 'mgmt\\(2\\)', 'oid' : '1.3.6.1.2.2'},
         {'name' : 'system(1)',  'parent' : 'mib\\(2\\)', 'oid' : '1.3.6.1.2.2.1'},
-        {'name' : 'interface(2)',  'parent' : 'mib\\(2\\)', 'oid' : '1.3.6.1.2.2.2'}];
-        
-        for(var i = 0; i < jsonArray.length; i++) 
+        {'name' : 'interface(2)',  'parent' : 'mib\\(2\\)', 'oid' : '1.3.6.1.2.2.2'}
+    ];
+    
+    /*generate tree*/
+    for(var i = 0; i < jsonArray.length; i++) 
+    {
+        if(i == 0)
         {
-            if(i == 0)
-            {
-                var nodes = '<li>';
-                nodes += "<a id=" + jsonArray[i].oid + ">";
-                nodes += jsonArray[i].name; 
-                nodes += '</a>';
-                nodes += '</li>';  
-                $("#iso\\(1\\)").append(nodes); 
-            }
-            else 
-            {
-                nodes = "<ul class='node' id=" + jsonArray[i].name + ">";
-                nodes += '<li>';
-                nodes += "<a id=" + jsonArray[i].oid + ">";
-                nodes += jsonArray[i].name; 
-                nodes += '</a>';
-                nodes += '</li>';
-                nodes += '</ul>'; 
-                var hashVar = "#" + jsonArray[i].parent; 
-                $(hashVar).children('li').append(nodes);
-            }
+            var nodes = '<li>';
+            nodes += "<a id=" + jsonArray[i].oid + ">";
+            nodes += jsonArray[i].name; 
+            nodes += '</a>';
+            nodes += '</li>';  
+            $("#iso\\(1\\)").append(nodes); 
         }
+        else 
+        {
+            nodes = "<ul class='node' id=" + jsonArray[i].name + ">";
+            nodes += '<li>';
+            nodes += "<a id=" + jsonArray[i].oid + ">";
+            nodes += jsonArray[i].name; 
+            nodes += '</a>';
+            nodes += '</li>';
+            nodes += '</ul>'; 
+            var hashVar = "#" + jsonArray[i].parent; 
+            $(hashVar).children('li').append(nodes);
+        }
+    }
 
     /*hide tree*/   
     $('#iso\\(1\\)').find('ul').hide();
@@ -50,6 +52,11 @@ $(document).ready(function(){
         $('#oid').attr({value:id});
 	   $(this).parent('li').children('ul').toggle();
     }); 
+
+    /*show all*/
+    $("#showAll").click(function () {
+         $('#iso\\(1\\)').find('ul').show();
+    });
 
     /* create URL */
     $('#input_form').submit(function() {
