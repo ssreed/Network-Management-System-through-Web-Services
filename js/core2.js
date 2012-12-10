77d9da3114b20cbfabb3ea8d9ef11c20119cc40d
Thanh Au <thanhau123@yahoo.com>
2012-12-10 00:18:19 -0800

    work on monitor status

--- js/core2.js
+++ js/core2.js
@@ -247,7 +247,34 @@ $(document).ready(function(){
             });
             table = table + '</table>';
             $("#result").append(table);
-        }  // else if
+        }  
+		 else if ($('select#commands').val() === 'status')  {
+			 $.each(data, function (pIndex, pElement) {
+                var lName = { text: pElement.name };
+                if (lName.text == "OID Name") {
+                    var lValue = { text: pElement.value };
+
+                    var lLI = document.createElement("li");
+                    $("#node_list").append(lLI);
+                    $(lLI).append($("<p>", lValue));
+                }  // if
+                else if (lName.text == "STATUS") {
+                    var lValue = { text: pElement.value };
+
+                    var lLI = document.createElement("li");
+                    $("#node_list").append(lLI);
+                    $(lLI).append($("<p>", lValue));
+                }  // if
+                else {
+                    var lValue = { text: pElement.value };
+                    var lLI = document.createElement("li");
+                    $("#node_list").append(lLI);
+                    $(lLI).append($("<p>", lName));
+                    $(lLI).append($("<p>", lValue));
+                }  // else
+            });
+		 }
+		// else if
         else {
 		$.each(data, function (pIndex, pElement) {
 			var result  = '<p>' + pElement.name + ':' + pElement.value + '</p>';
